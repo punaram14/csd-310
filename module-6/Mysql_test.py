5b37b22d130ce80698eed881c2778cc8811b88d4
puna <prp8334@gmail.com>
2024-07-03 02:02:15 -0500

    module 6 assignment uploaded

---
+++ module-6/Mysql_test.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+config = {
+  'user': 'root',
+  'password': 'root',
+  'host': '127.0.0.1',
+  'database': 'movies',
+  'raise_on_warnings': True
+}
+
+cnx = mysql.connector.connect(**config)
+print("connected")
+cursor = cnx.cursor();
+cursor.execute("show databases");
+for x in cursor:
+  print(x)
+
+
+cnx.close()
+
+
+
+
+
